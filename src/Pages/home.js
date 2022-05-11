58916149784e70fc622ebbb43f13a278f5abb90d
esteban <esteban030991@gmail.com>
2022-05-11 14:35:18 -0300

    creating and separating broken

---
+++ src/Pages/home.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Container, Paper, Grid, Typography, Button, List, ListItem, ListItemText, makeStyles,} from "@material-ui/core/";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+      flexGrow: 1,
+      marginTop: "5px",
+  },
+  paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+  },
+}));
+const HomePage = () => {
+  const classes = useStyles();
+    return (
+        <Container maxWidth="xl">
+            <Grid container spacing={3} className={classes.root}>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <Typography variant="h5">Categorias</Typography>
+                        <List>
+                            <ListItem>
+                                <ListItemText primary="Times nacionais" secondary="3" />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Times internacionais" secondary="3" />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Times históricos" secondary="3" />
+                            </ListItem>
+                        </List>
+                    </Paper>
+                </Grid>
+                <Grid container xs={9} spacing={3} className={classes.root}>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <img
+                                        width="140px"
+                                        src="./images/produtos/fla.jpg"
+                                        alt="Flamengo 2020"
+                                    />
+                                    <Typography variant="h6">Produto</Typography>
+                                    <Typography variant="subtitle1">R$ 99.00</Typography>
+                                </Grid>
+                                <Button variant="contained">Adicionar</Button>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <img
+                                        width="140px"
+                                        src="./images/produtos/fla.jpg"
+                                        alt="Flamengo 2020"
+                                    />
+                                    <Typography variant="h6">Produto</Typography>
+                                    <Typography variant="subtitle1">R$ 99.00</Typography>
+                                </Grid>
+                                <Button variant="contained">Adicionar</Button>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <img
+                                        width="140px"
+                                        src="./images/produtos/fla.jpg"
+                                        alt="Flamengo 2020"
+                                    />
+                                    <Typography variant="h6">Produto</Typography>
+                                    <Typography variant="subtitle1">R$ 99.00</Typography>
+                                </Grid>
+                                <Button variant="contained">Adicionar</Button>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <img
+                                        width="140px"
+                                        src="./images/produtos/fla.jpg"
+                                        alt="Flamengo 2020"
+                                    />
+                                    <Typography variant="h6">Produto</Typography>
+                                    <Typography variant="subtitle1">R$ 99.00</Typography>
+                                </Grid>
+                                <Button variant="contained">Adicionar</Button>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default HomePage;
---
+++ src/routas.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/home';   
+import Contatos from './Pages/contato';    
+
+const MainRoute = () => {
+       return (
+              <Routes>
+                     <Route
+                         path="/"
+                         element={<HomePage />}
+                     />
+                     <Route
+                         path="contato"   
+                         element={<Contatos />}
+                     />
+              </Routes>
+       )
+
+}
+
+export default MainRoute;
